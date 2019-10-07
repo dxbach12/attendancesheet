504b7d0c3351c3eed72165e64b3f3b92308a7977
Bach Dao <dxbach12@gmail.com>
2019-10-07 10:24:31 -0500

    a

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var mongodb = require('mongodb');
+var bodyParser = require('body-parser');
+​
+var OBJECTID = mongodb.ObjectID;
+​
+var CLASS_COLLECTION = "test_attendance";
+​
+var app = express();
+app.use(bodyParser.json());
+​
+var db;
+​
+mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/test", function(err, client){
+  if(err){
+    console.log(err);
+    process.exit(1);
+  }
+​
+  db = client.db();
+  console.log("Database connection successfull ");
+​
+  var server = app.listen(process.env.PORT || 8080, function(){
+    var port = server.address().port;
+    console.log("App now running on port " + port);
+  });
+​
+});
